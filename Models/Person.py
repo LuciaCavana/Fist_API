e4473c1383c9156a5d787323f404c4315a7b64ed
Lucia Cavana <luciacavana@yahoo.com.ar>
2022-12-24 14:41:52 -0300

    Remove Duplicate

--- Models/Person.py
+++ Models/Person.py
@@ -3,8 +3,7 @@ from .HairColor import HairColor as HC
 from pydantic import BaseModel
 from pydantic import Field, EmailStr, PositiveFloat
 
-#Generate Person
-class Person(BaseModel):
+class PersonBase(BaseModel):
     person_id:int=Field(..., ge=0,title="Person ID", description="This is Person ID")
     first_name:str=Field(..., max_length=30, min_length=1, title="First name", description="This is the person first name")
     last_name:str=Field(..., max_length=30, min_length=1, title="Last name", description="This is the person last name")
@@ -13,6 +12,12 @@ class Person(BaseModel):
     hair_color: Optional[HC]=Field(default=None, title="Person hair color", description="This is person hair color")
     person_email: EmailStr = Field(default=None, title="Person email", description="This is person email")
     earnings: PositiveFloat = Field(default=None, title="Person earnings", description="This is person earnings")
+
+
+
+
+#Generate Person
+class Person(PersonBase):
     password: str=Field(...,max_length=16,min_length=8, title="Passwor", description="This Password requires a minimum of 8 characters and maximum of 16")
     class Config:
         schema_extra={
@@ -25,31 +30,11 @@ class Person(BaseModel):
                 "is_married": False,
                 "person_email": "luciacavana@yahoo.com",
                 "earnings": 120000,
-                "password": "admin123"
+                "password":"admin123"
             }
         }
 
+
 #Person Out
-class PersonOut(BaseModel):
-    person_id:int=Field(..., ge=0,title="Person ID", description="This is Person ID")
-    first_name:str=Field(..., max_length=30, min_length=1, title="First name", description="This is the person first name")
-    last_name:str=Field(..., max_length=30, min_length=1, title="Last name", description="This is the person last name")
-    age: int = Field(..., gt=1, lt=115, title="Person age", description="This is person age")
-    is_married:bool=Field(default=None, title="", description="")
-    hair_color: Optional[HC]=Field(default=None, title="Person hair color", description="This is person hair color")
-    person_email: EmailStr = Field(default=None, title="Person email", description="This is person email")
-    earnings: PositiveFloat = Field(default=None, title="Person earnings", description="This is person earnings")
-    
-    class Config:
-        schema_extra={
-            "example":{
-                "person_id": "1",
-                "first_name": "Lucia",
-                "last_name": "Cavana",
-                "age": 22,
-                "hair_color": "brown",
-                "is_married": False,
-                "person_email": "luciacavana@yahoo.com",
-                "earnings": 120000
-            }
-        }
+class PersonOut(PersonBase):
+    pass 
